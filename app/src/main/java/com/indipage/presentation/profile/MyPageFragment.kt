532a3fff767a3366535289405c708908d948206f
Sohyun <so405@naver.com>
2023-07-06 16:28:40 +0900

    [ADD] add-navigation / #4
    MyPageFragment, fragment_my_page 파일 추가

---
+++ app/src/main/java/com/indipage/presentation/profile/MyPageFragment.kt
@@ -0,0 +1,16 @@
+package com.indipage.presentation.profile
+
+import android.os.Bundle
+import android.view.View
+import com.indipage.R
+import com.indipage.databinding.FragmentMyPageBinding
+import com.indipage.util.base.BindingFragment
+
+class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
