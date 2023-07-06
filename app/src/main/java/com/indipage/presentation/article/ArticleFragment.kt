8da68a263027e2f3dd228d9275a1c3b8948cc68e
Sohyun <so405@naver.com>
2023-07-06 16:18:17 +0900

    [ADD] : add-navigation / #4
    ArticleFragment, fragment_article 파일 추가

---
+++ app/src/main/java/com/indipage/presentation/article/ArticleFragment.kt
@@ -0,0 +1,16 @@
+package com.indipage.presentation.article
+
+import android.os.Bundle
+import android.view.View
+import com.indipage.R
+import com.indipage.databinding.FragmentArticleBinding
+import com.indipage.util.base.BindingFragment
+
+class ArticleFragment : BindingFragment<FragmentArticleBinding>(R.layout.fragment_article) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
