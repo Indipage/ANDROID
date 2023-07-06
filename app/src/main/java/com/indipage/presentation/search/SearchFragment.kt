34e84501897d2ca77fdfd2fc3e25865f4082faf0
Sohyun <so405@naver.com>
2023-07-06 16:22:43 +0900

    [ADD] : add-navigation / #4
    SearchFragment, fragment_search 파일 추가

---
+++ app/src/main/java/com/indipage/presentation/search/SearchFragment.kt
@@ -0,0 +1,16 @@
+package com.indipage.presentation.search
+
+import android.os.Bundle
+import android.view.View
+import com.indipage.R
+import com.indipage.databinding.FragmentSearchBinding
+import com.indipage.util.base.BindingFragment
+
+class SearchFragment : BindingFragment<FragmentSearchBinding>(R.layout.fragment_search) {
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
