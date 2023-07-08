06a506e5b9629b1ed2156340bca3aa073f8166ad
issac <lsls4868@gmail.com>
2023-07-08 17:06:21 +0900

    [CHORE] : 다크모드 비활 다크모드 막기

--- app/src/main/java/com/indipage/IndiPageApp.kt
+++ app/src/main/java/com/indipage/IndiPageApp.kt
@@ -1,6 +1,7 @@
 package com.indipage
 
 import android.app.Application
+import androidx.appcompat.app.AppCompatDelegate
 import dagger.hilt.android.HiltAndroidApp
 
 
@@ -8,6 +9,7 @@ import dagger.hilt.android.HiltAndroidApp
 class IndiPageApp : Application() {
     override fun onCreate() {
         super.onCreate()
+        AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
     }
 
 }
