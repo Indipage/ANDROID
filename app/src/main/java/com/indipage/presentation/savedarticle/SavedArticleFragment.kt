e7ae73f9b5442b4bf151cee1b56f318eaf2cd42c
issac <lsls4868@gmail.com>
2023-07-10 17:10:34 +0900

    [ADD] : init Saved Article #12

---
+++ app/src/main/java/com/indipage/presentation/savedarticle/SavedArticleFragment.kt
@@ -0,0 +1,14 @@
+package com.indipage.presentation.savedarticle
+
+import android.os.Bundle
+import android.view.View
+import com.example.core_ui.base.BindingFragment
+import com.indipage.R
+import com.indipage.databinding.FragmentSavedArticleBinding
+
+class SavedArticleFragment :
+    BindingFragment<FragmentSavedArticleBinding>(R.layout.fragment_saved_article) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+    }
+}
