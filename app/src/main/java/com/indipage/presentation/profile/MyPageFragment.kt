67b6b66e11aa49e58285114615e295c776320b48
issac <lsls4868@gmail.com>
2023-07-10 23:26:00 +0900

    [FEAT] : 화면이동구현 #12

--- app/src/main/java/com/indipage/presentation/profile/MyPageFragment.kt
+++ app/src/main/java/com/indipage/presentation/profile/MyPageFragment.kt
@@ -2,6 +2,8 @@ package com.indipage.presentation.profile
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 import com.example.core_ui.base.BindingFragment
 import com.indipage.R
 import com.indipage.databinding.FragmentMyPageBinding
@@ -11,6 +13,14 @@ import dagger.hilt.android.AndroidEntryPoint
 class MyPageFragment : BindingFragment<FragmentMyPageBinding>(R.layout.fragment_my_page) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.tvProfile.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_navigation_my_page_to_saved_article, bundleOf(
+                )
+            )
+        }
+
     }
+
 
 }
