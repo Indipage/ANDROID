b422c3f3dfe62a911e46472a0b66164f2253afdd
issac <lsls4868@gmail.com>
2023-07-10 23:25:37 +0900

    [REFACTOR] : page,size 로인한 리팩토링 #12

--- app/src/main/java/com/indipage/presentation/savedarticle/SavedArticleAdapter.kt
+++ app/src/main/java/com/indipage/presentation/savedarticle/SavedArticleAdapter.kt
@@ -3,33 +3,37 @@ package com.indipage.presentation.savedarticle
 import android.view.LayoutInflater
 import android.view.ViewGroup
 import androidx.paging.PagingDataAdapter
+import androidx.recyclerview.widget.ListAdapter
 import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
 import com.example.core_ui.view.ItemDiffCallback
 import com.indipage.data.dto.response.SavedArticle
 import com.indipage.databinding.ItemSavedArticleBinding
 
 class SavedArticleAdapter(
-) : PagingDataAdapter<SavedArticle, SavedArticleAdapter.PagingViewHolder>(
+) : ListAdapter<SavedArticle, SavedArticleAdapter.SavedArticleViewHolder>(
     SavedArticleDiffCalback
 ) {
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PagingViewHolder {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SavedArticleViewHolder {
         val binding =
             ItemSavedArticleBinding.inflate(LayoutInflater.from(parent.context), parent, false)
-        return PagingViewHolder(binding)
+        return SavedArticleViewHolder(binding)
     }
 
-    override fun onBindViewHolder(holder: PagingViewHolder, position: Int) {
-        getItem(position)?.let { holder.bind(it) }
+    override fun onBindViewHolder(holder: SavedArticleViewHolder, position: Int) {
+//        getItem(position)?.let { holder.bind(it) }
+        holder.bind(getItem(position))
     }
-
-    inner class PagingViewHolder(private val binding: ItemSavedArticleBinding) :
-        RecyclerView.ViewHolder(binding.root) {
+    class SavedArticleViewHolder(
+        private val binding: ItemSavedArticleBinding
+    ):ViewHolder(binding.root){
         fun bind(data: SavedArticle) {
             binding.savedArticle = data
             binding.executePendingBindings()
         }
     }
+
 
     companion object {
         private val SavedArticleDiffCalback =
