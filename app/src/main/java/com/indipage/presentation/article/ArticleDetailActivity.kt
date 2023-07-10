f4980502db9bce539f61881e807f15871a4ddc96
Sohyun <so405@naver.com>
2023-07-11 00:30:41 +0900

    [ADD] : article-detail-ui / #14
    파일 추가

---
+++ app/src/main/java/com/indipage/presentation/article/ArticleDetailActivity.kt
@@ -0,0 +1,15 @@
+package com.indipage.presentation.article
+
+import com.example.core_ui.base.BindingActivity
+import com.indipage.R
+import com.indipage.databinding.ActivityArticleDetailBinding
+
+class ArticleDetailActivity:BindingActivity<ActivityArticleDetailBinding>(R.layout.activity_article_detail) {
+
+
+    override fun initView() {
+        TODO("Not yet implemented")
+    }
+
+
+}
