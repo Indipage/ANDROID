12cf150dc4c240852ee1f3feed59df9e7ac661e3
issac <lsls4868@gmail.com>
2023-07-11 00:29:28 +0900

    [UI] : 북마크한 공간 #15

--- app/src/main/java/com/indipage/data/api/BookMarkApiService.kt
+++ app/src/main/java/com/indipage/data/api/BookMarkApiService.kt
@@ -2,9 +2,13 @@ package com.indipage.data.api
 
 import com.indipage.data.dto.BaseResponse
 import com.indipage.data.dto.response.SavedArticle
+import com.indipage.data.dto.response.SavedSpace
 import retrofit2.http.GET
 
 interface BookMarkApiService {
     @GET("/user/bookmark/article")
     suspend fun getSavedArticles(): BaseResponse<List<SavedArticle>>
+
+    @GET("/user/bookmark/article")
+    suspend fun getSavedSpaces(): BaseResponse<List<SavedSpace>>
 }
--- app/src/main/java/com/indipage/data/datasource/BookMarkDataSource.kt
+++ app/src/main/java/com/indipage/data/datasource/BookMarkDataSource.kt
@@ -2,7 +2,9 @@ package com.indipage.data.datasource
 
 import com.indipage.data.dto.BaseResponse
 import com.indipage.data.dto.response.SavedArticle
+import com.indipage.data.dto.response.SavedSpace
 
 interface BookMarkDataSource {
     suspend fun getSavedArticles(): BaseResponse<List<SavedArticle>>
+    suspend fun getSavedSpaces(): BaseResponse<List<SavedSpace>>
 }
