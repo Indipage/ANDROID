0a1f2eb636ff193db2cf10884a86f70f42d92b9b
issac <lsls4868@gmail.com>
2023-07-13 03:19:46 +0900

    [FEAT] : 데이터 소스 di  #29

--- app/src/main/java/com/indipage/di/DataSourceModule.kt
+++ app/src/main/java/com/indipage/di/DataSourceModule.kt
@@ -2,9 +2,11 @@ package com.indipage.di
 
 import com.indipage.data.datasource.BookMarkDataSource
 import com.indipage.data.datasource.TestDataSource
+import com.indipage.data.datasource.TicketDataSource
 import com.indipage.data.datasource.UserDataSource
 import com.indipage.data.datasource.remote.BookMarkDataSourceImpl
 import com.indipage.data.datasource.remote.TestDataSourceImpl
+import com.indipage.data.datasource.remote.TicketDataSourceImpl
 import com.indipage.data.datasource.remote.UserDataSourceImpl
 import dagger.Binds
 import dagger.Module
@@ -27,5 +29,9 @@ abstract class DataSourceModule {
     @Singleton
     @Binds
     abstract fun providesUserDataSource(DataSourceImpl: UserDataSourceImpl): UserDataSource
+
+    @Singleton
+    @Binds
+    abstract fun providesTicketDataSource(DataSourceImpl: TicketDataSourceImpl): TicketDataSource
 
 }
