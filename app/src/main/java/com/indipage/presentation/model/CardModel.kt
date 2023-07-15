4fa0d9d67a2f2b886708f8d66559bf1cb77dd261
issac <lsls4868@gmail.com>
2023-07-15 18:42:49 +0900

    [ADD]: 카드 모델 데이터 생성 #48

---
+++ app/src/main/java/com/indipage/presentation/model/CardModel.kt
@@ -0,0 +1,14 @@
+package com.indipage.presentation.model
+
+import com.indipage.domain.entity.Card
+
+data class CardModel (
+    val cardId :Long,
+    val imageUrl:String,
+    val visitedAt:String,
+    val spaceId:String,
+)
+
+fun Card.toCardModelEntity()= CardModel(
+    cardId, imageUrl, visitedAt, spaceId
+)
