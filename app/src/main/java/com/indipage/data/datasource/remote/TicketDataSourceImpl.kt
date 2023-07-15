df20404a2d9b8492544bb921251776518e0ae0e1
issac <lsls4868@gmail.com>
2023-07-15 18:48:11 +0900

    [FEAT]: 데이터 소스 구현완료  #48

--- app/src/main/java/com/indipage/data/datasource/remote/TicketDataSourceImpl.kt
+++ app/src/main/java/com/indipage/data/datasource/remote/TicketDataSourceImpl.kt
@@ -4,6 +4,7 @@ import com.indipage.data.api.TicketApiService
 import com.indipage.data.datasource.TicketDataSource
 import com.indipage.data.dto.BaseResponse
 import com.indipage.data.dto.QRResponse
+import com.indipage.data.dto.response.ResponseCardDto
 import com.indipage.data.dto.response.ResponseTicketDto
 import javax.inject.Inject
 
@@ -11,12 +12,16 @@ class TicketDataSourceImpl @Inject constructor(
     private val apiService: TicketApiService
 ) : TicketDataSource {
 
-    override suspend fun isCheckQR(spaceId :Int): QRResponse {
+    override suspend fun isCheckQR(spaceId: Int): QRResponse {
         return apiService.isCheckQR(spaceId)
     }
 
     override suspend fun getTicketList(): BaseResponse<ResponseTicketDto> {
         return apiService.getTicketList()
+    }
+
+    override suspend fun getCardList(): BaseResponse<ResponseCardDto> {
+        return apiService.getCardList()
     }
 
 }
