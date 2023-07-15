6ec794221674d5b3f6d0c8511245e34416d4b5af
issac <lsls4868@gmail.com>
2023-07-15 18:48:36 +0900

    [FEAT]: Card api 연동  #48

--- app/src/main/java/com/indipage/data/repositoryimpl/TicketRepositoryImpl.kt
+++ app/src/main/java/com/indipage/data/repositoryimpl/TicketRepositoryImpl.kt
@@ -2,7 +2,9 @@ package com.indipage.data.repositoryimpl
 
 import com.indipage.data.datasource.TicketDataSource
 import com.indipage.data.dto.BaseResponse
+import com.indipage.data.dto.response.toCardEntity
 import com.indipage.data.dto.response.toTicketEntity
+import com.indipage.domain.entity.Card
 import com.indipage.domain.entity.Ticket
 import com.indipage.domain.repository.TicketRepository
 import javax.inject.Inject
@@ -20,6 +22,12 @@ class TicketRepositoryImpl @Inject constructor(
     override suspend fun getTicketList(): Result<Ticket> {
         return kotlin.runCatching {
             dataSource.getTicketList().data.toTicketEntity()
+        }
+    }
+
+    override suspend fun getCardList(): Result<Card> {
+        return kotlin.runCatching {
+            dataSource.getCardList().data.toCardEntity()
         }
     }
 }
