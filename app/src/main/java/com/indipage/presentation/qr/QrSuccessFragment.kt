5af71565fa4fcd9e5576a26c0fa87bc8f767b676
issac <lsls4868@gmail.com>
2023-07-18 20:27:28 +0900

    [FEAT]: qr success move#68

--- app/src/main/java/com/indipage/presentation/qr/QrSuccessFragment.kt
+++ app/src/main/java/com/indipage/presentation/qr/QrSuccessFragment.kt
@@ -2,6 +2,8 @@ package com.indipage.presentation.qr
 
 import android.os.Bundle
 import android.view.View
+import androidx.core.os.bundleOf
+import androidx.navigation.fragment.findNavController
 import com.example.core_ui.base.BindingFragment
 import com.indipage.R
 import com.indipage.databinding.FragmentQrSuccessBinding
@@ -9,5 +11,12 @@ import com.indipage.databinding.FragmentQrSuccessBinding
 class QrSuccessFragment : BindingFragment<FragmentQrSuccessBinding>(R.layout.fragment_qr_success) {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        binding.btnQrSuccessMoveCard.setOnClickListener {
+            findNavController().navigate(
+                R.id.action_navigation_qr_success_to_navigation_card,
+                bundleOf()
+            )
+        }
     }
 }
