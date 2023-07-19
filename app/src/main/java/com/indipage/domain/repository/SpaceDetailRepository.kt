2230755bfcfdf863dafc93aa00469263a7a81313
issac <lsls4868@gmail.com>
2023-07-19 19:52:46 +0900

    [Refactor]: 레포지토리 소스 수정 #81

--- app/src/main/java/com/indipage/domain/repository/SpaceDetailRepository.kt
+++ app/src/main/java/com/indipage/domain/repository/SpaceDetailRepository.kt
@@ -7,13 +7,13 @@ import com.indipage.data.dto.response.SpaceArticleData
 import com.indipage.data.dto.response.SpaceDetailData
 
 interface SpaceDetailRepository {
-    suspend fun getSpaceDetail(): Result<SpaceDetailData>
-    suspend fun getCuration(): Result<List<CurationData>>
-    suspend fun getBookmarked(): Result<BookmarkData>
-    suspend fun postBookmarked(): Result<Int>
-    suspend fun deleteBookmarked(): Result<Int>
-    suspend fun getFollow(): Result<FollowData>
-    suspend fun postFollow(): Result<Int>
-    suspend fun getSpaceArticle(): Result<SpaceArticleData?>
+    suspend fun getSpaceDetail(spaceId:Int): Result<SpaceDetailData>
+    suspend fun getCuration(spaceId:Int): Result<List<CurationData>>
+    suspend fun getBookmarked(spaceId:Int): Result<BookmarkData>
+    suspend fun postBookmarked(spaceId:Int): Result<Int>
+    suspend fun deleteBookmarked(spaceId:Int): Result<Int>
+    suspend fun getFollow(spaceId:Int): Result<FollowData>
+    suspend fun postFollow(spaceId:Int): Result<Int>
+    suspend fun getSpaceArticle(spaceId:Int): Result<SpaceArticleData?>
 
 }
