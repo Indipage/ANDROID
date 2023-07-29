3453a7da3d49cac09c1479cac2e7bc24b484f78d
lsakee <93514333+lsakee@users.noreply.github.com>
2023-07-30 01:48:58 +0900

    [ADD] :  presentation module 생성 #122

---
+++ presentation/build.gradle.kts
@@ -0,0 +1,84 @@
+plugins {
+    id("com.android.library")
+    kotlin("android")
+    kotlin("kapt")
+    kotlin("plugin.serialization") version Versions.KOTLIN_VERSION
+    // Hilt
+    // 앱잼 끝나고 구현하자!
+    id("dagger.hilt.android.plugin")
+}
+
+android {
+    namespace = Configuration.PACKAGE_NAME
+    compileSdk = Configuration.COMPILE_SDK
+
+    defaultConfig {
+        minSdk = Configuration.MIN_SDK
+        targetSdk = Configuration.TARGET_SDK
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        consumerProguardFiles("consumer-rules.pro")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = Versions.JAVA_VERSION
+        targetCompatibility = Versions.JAVA_VERSION
+    }
+    kotlinOptions {
+        jvmTarget = Versions.JVM_VERSION
+    }
+    buildFeatures {
+        dataBinding = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+
+    implementation(project(":core-ui"))
+    implementation(project(":domain"))
+    // Kotlin
+    implementation(KotlinX.KOTLINX_SERIALIZATION)
+    // AndroidX
+    implementation(AndroidX.ACTIVITY)
+    implementation(AndroidX.APP_COMPAT)
+    implementation(AndroidX.CORE_KTX)
+    implementation(AndroidX.LIFECYCLE_RUNTIME)
+    implementation(AndroidX.PAGING)
+    implementation(AndroidX.LIFECYCLE_VIEWMODEL_KTX)
+    implementation(AndroidX.NAVIGATION_CONPONENT_FRAGMENT)
+    implementation(AndroidX.NAVIGATION_CONPONENT_UI)
+    implementation(AndroidX.CONSTRAINT_LAYOUT)
+
+    // Matrial Design
+    implementation(Google.MATERIAL)
+
+    // Test Dependency
+    androidTestImplementation(TestDependencies.EXT_JUNIT)
+    androidTestImplementation(TestDependencies.ESPRESSO_CORE)
+    testImplementation(TestDependencies.JUNIT)
+
+    //Hilt
+    implementation(Google.HILT_ANDROID)
+    kapt(Google.HILT_ANDROID_COMPILER)
+
+    // Third-Party
+    implementation(SquareUp.RETROFIT2)
+    implementation(SquareUp.RETROFIT2_CONVERTER_GSON)
+    implementation(SquareUp.OKHTTP3)
+    implementation(SquareUp.OKHTTP3_LOGGING)
+    implementation(SquareUp.OKHTTP3_BOM)
+    implementation(Jakewharton.TIMBER)
+    implementation(Jakewharton.CONVERTER)
+    implementation(ThirdParty.COIL)
+    implementation(ThirdParty.ZXING)
+}
