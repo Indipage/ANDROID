681435f72b1058fca93f78d303a26b1a551888b1
sommac <salth6@naver.com>
2023-08-13 12:25:11 +0900

    [ADD] SearchUseCase 추가

---
+++ domain/src/main/java/com/indipage/domain/usecase/SearchUseCase.kt
@@ -0,0 +1,12 @@
+package com.indipage.domain.usecase
+
+
+import com.indipage.domain.model.Search
+import com.indipage.domain.repository.SearchRepository
+
+class SearchUseCase(
+    private val repository: SearchRepository
+) {
+    suspend fun getSearchResult(keyword: String): Result<List<Search>?> =
+        repository.getSearchResult(keyword)
+}
