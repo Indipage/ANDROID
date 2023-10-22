e4ad9e7af94f9db132932dabc62808de5cfc8682
lsakee <93514333+lsakee@users.noreply.github.com>
2023-10-22 21:56:59 +0900

    [FIX] : Token 수정

--- buildSrc/src/main/java/Configuration.kt
+++ buildSrc/src/main/java/Configuration.kt
@@ -3,6 +3,6 @@ object Configuration {
     const val COMPILE_SDK = 33
     const val TARGET_SDK = 33
     const val MIN_SDK = 26
-    const val VERSION_CODE = 1
+    const val VERSION_CODE = 2
     const val VERSION_NAME = "1.0"
 }
--- data/src/main/java/com/indipage/data/repositoryimpl/AuthRepositoryImpl.kt
+++ data/src/main/java/com/indipage/data/repositoryimpl/AuthRepositoryImpl.kt
@@ -1,13 +1,11 @@
 package com.indipage.data.repositoryimpl
 
 import com.indipage.data.datasource.TokenImpl
-import com.indipage.domain.SharedPreferenceToken
 import com.indipage.domain.repository.AuthRepository
 import javax.inject.Inject
 
 class AuthRepositoryImpl @Inject constructor(
     private val sharedPrefDataSource: TokenImpl,
-    private val token: SharedPreferenceToken
 ) : AuthRepository {
     override fun saveAccessToken(accessToken: String) {
         sharedPrefDataSource.token = accessToken
