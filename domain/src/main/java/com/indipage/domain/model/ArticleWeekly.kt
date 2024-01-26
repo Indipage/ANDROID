5fab18ca180a5ee454738eaad246a61982b1b621
sohyun127 <so405@naver.com>
2024-01-26 21:19:00 +0900

    [FEAT] : 공간지기 nullable로 변경 / #156

--- domain/src/main/java/com/indipage/domain/model/ArticleWeekly.kt
+++ domain/src/main/java/com/indipage/domain/model/ArticleWeekly.kt
@@ -4,7 +4,7 @@ data class ArticleWeekly(
     val id: Int,
     val title: String,
     val spaceName: String,
-    val spaceOwner: String,
+    val spaceOwner: String? = null,
     val remainingDays: Int,
     val thumbnailUrlOfThisWeek: String,
     val thumbnailUrlOfNextWeek: String
